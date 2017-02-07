4c520e2c70eb5804c239dea6e075884ccc5db577
Distiller <distiller@distiller-build-48.local>
2017-02-07 01:55:25 -0800

    Release 1.2

--- iOS/Twisto.framework/Versions/Current/Headers/Twisto.h
+++ iOS/Twisto.framework/Versions/Current/Headers/Twisto.h
@@ -11,17 +11,20 @@
 @class Twisto;
 @protocol TwistoCheckDelegate <NSObject>
 @required
+NS_ASSUME_NONNULL_BEGIN
 - (void) TwistoCheckAccepted:(NSString *)transactionId;
 - (void) TwistoCheckRejected:(NSString *)transactionId reason:(NSString *)reason;
 - (void) TwistoCheckError:(NSError *)error;
+NS_ASSUME_NONNULL_END
 @end
 
 @interface Twisto : NSObject
 
-@property (nonatomic, weak) id <TwistoCheckDelegate> delegate;
-- (id)initWithPublicKey:(NSString *)key;
-- (id)initWithPublicKey:(NSString *)key withDelegate: (id)delegate;
+@property (nonatomic, weak, nullable) id <TwistoCheckDelegate> delegate;
+NS_ASSUME_NONNULL_BEGIN
+- (nullable instancetype)initWithPublicKey:(NSString *)key;
+- (nullable instancetype)initWithPublicKey:(NSString *)key withDelegate: (id<TwistoCheckDelegate>)delegate;
 - (void)checkWithPayload: (NSData *)payload;
 - (void)checkWithPayload:(NSData *)data acceptedCompletionHandler: (void(^)(NSString * transactionId))acceptedCompletionHandler rejectedCompletionHandler: (void(^)(NSString * transactionId, NSString  * reason))rejectedCompletionHandler errorCompletionHandler: (void(^ )(NSError * error))errorCompletionHandler;
-
+NS_ASSUME_NONNULL_END
 @end
