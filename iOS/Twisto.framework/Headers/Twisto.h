e6d61075fe7cf93a4072d1e9aeb479e103307884
Distiller <distiller@static.162.252.209.204.cyberlynk.net>
2018-11-15 07:59:01 -0800

    Release 1.5.7

--- iOS/Twisto.framework/Headers/Twisto.h
+++ iOS/Twisto.framework/Headers/Twisto.h
@@ -28,6 +28,6 @@ NS_ASSUME_NONNULL_BEGIN
 - (nonnull instancetype)initWithPublicKey:(NSString *)key;
 - (nonnull instancetype)initWithPublicKey:(NSString *)key withDelegate: (id<TwistoCheckDelegate>)delegate;
 - (void)checkWithPayload: (NSData *)payload;
-- (void)checkWithPayload:(NSData *)data acceptedCompletionHandler: (void(^)(NSString * transactionId))acceptedCompletionHandler rejectedCompletionHandler: (void(^)(NSString * transactionId, NSString  * reason))rejectedCompletionHandler errorCompletionHandler: (void(^)(NSError * error))errorCompletionHandler willDisplayLoginDialogHandler: ( void(^ _Nullable )())willDisplayLoginDialogHandler  didHideLoginDialogHandler: (void(^ _Nullable )())didHideLoginDialogHandler;
+- (void)checkWithPayload:(NSData *)data acceptedCompletionHandler: (void(^)(NSString * transactionId))acceptedCompletionHandler rejectedCompletionHandler: (void(^)(NSString * transactionId, NSString  * reason))rejectedCompletionHandler errorCompletionHandler: (void(^)(NSError * error))errorCompletionHandler willDisplayLoginDialogHandler: ( void(^ _Nullable )(void))willDisplayLoginDialogHandler  didHideLoginDialogHandler: (void(^ _Nullable )(void))didHideLoginDialogHandler;
 NS_ASSUME_NONNULL_END
 @end
