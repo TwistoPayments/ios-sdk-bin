900754c69b8479f260f3609944e11dbf5821323a
Petr Slovacek <pslovacek@gmail.com>
2022-02-04 16:53:20 +0100

    Release 1.6.0

--- Twisto.podspec
+++ Twisto.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Twisto'
-  s.version          = '1.5.8'
+  s.version          = '1.6.0'
   s.summary          = 'Twisto iOS SDK framework'
 
   s.description      = <<-DESC
@@ -22,13 +22,11 @@ Copyright 2016 Twisto Payments a.s. All rights reserved.
                             LICENSE
   }
   s.author           = { 'Twisto Payments a.s.' => 'devs@twisto.cz' }
-  s.source           = { :git => 'https://github.com/TwistoPayments/ios-sdk-bin.git', :tag => '1.5.8' }
-  s.source_files       = 'iOS/Twisto.framework/Headers/Twisto.h'
+  s.source           = { :git => 'https://github.com/TwistoPayments/ios-sdk-bin.git', :tag => '1.6.0' }
   s.platform = :ios
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '12.0'
 
-  s.ios.public_header_files = 'iOS/Twisto.framework/Headers/Twisto.h'
-  s.ios.vendored_frameworks = 'iOS/Twisto.framework'
+  s.ios.vendored_frameworks = 'iOS/Twisto.xcframework'
 
   s.preserve_path      = 'iOS/*'
   s.requires_arc = true
---
+++ iOS/Twisto.xcframework/ios-arm64_x86_64-simulator/Twisto.framework/Headers/Twisto.h
@@ -0,0 +1,33 @@
+//
+//  Twisto.h
+//  Twisto
+//
+//  Created by Petr Slovacek on 05.05.16.
+//  Copyright © 2016 Twisto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Twisto;
+@protocol TwistoCheckDelegate <NSObject>
+@required
+NS_ASSUME_NONNULL_BEGIN
+- (void) TwistoCheckAccepted:(NSString *)transactionId;
+- (void) TwistoCheckRejected:(NSString *)transactionId reason:(NSString *)reason;
+- (void) TwistoCheckError:(NSError *)error;
+@optional
+- (void) TwistoWillDisplayLoginDialog;
+- (void) TwistoDidHideLoginDialog;
+NS_ASSUME_NONNULL_END
+@end
+
+@interface Twisto : NSObject
+
+@property (nonatomic, weak, nullable) id <TwistoCheckDelegate> delegate;
+NS_ASSUME_NONNULL_BEGIN
+- (nonnull instancetype)initWithPublicKey:(NSString *)key;
+- (nonnull instancetype)initWithPublicKey:(NSString *)key withDelegate: (id<TwistoCheckDelegate>)delegate;
+- (void)checkWithPayload: (NSData *)payload;
+- (void)checkWithPayload:(NSData *)data acceptedCompletionHandler: (void(^)(NSString * transactionId))acceptedCompletionHandler rejectedCompletionHandler: (void(^)(NSString * transactionId, NSString  * reason))rejectedCompletionHandler errorCompletionHandler: (void(^)(NSError * error))errorCompletionHandler willDisplayLoginDialogHandler: ( void(^ _Nullable )(void))willDisplayLoginDialogHandler  didHideLoginDialogHandler: (void(^ _Nullable )(void))didHideLoginDialogHandler;
+NS_ASSUME_NONNULL_END
+@end
